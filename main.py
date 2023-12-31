bb4689a39373523c9fda7a13d7f81ecc107a7d17
connor <connord79@gmail.com>
2023-12-31 17:03:21 +1100

    Lives and Health

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import time
 import random
 
+pygame.font.init()
+
 WIDTH, HEIGHT = 600, 600
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -24,22 +26,40 @@ GREEN_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_green.png'))
 YELLOW_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_yellow.png'))
 
 # Background
-BG = pygame.image.load(os.path.join('assets', 'background-black.png'))
+BG = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'background-black.png')), (WIDTH, HEIGHT))
+
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health
+
+
+
 
 def main():
     run = True
     FPS = 60
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("comicsans", 50)
     clock = pygame.time.Clock()
 
     def redraw_window():
         WIN.blit(BG, (0, 0))
+        # Draw Text
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 0, 0))
+        level_label = main_font.render(f"Levels: {level}", 1, (255, 255, 255))
+
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
         pygame.display.update()
 
 
     while run:
         clock.tick(FPS)
-
+        redraw_window()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
